0565bf5c6079586c4c85d1310a07a41a2ceeb62e
Josué A <josue.alvarez@hotmail.fr>
2015-07-21 15:22:58 +0000

    Added cache system and better metrics support

--- clientapi/simsoexp/api.py
+++ clientapi/simsoexp/api.py
@@ -6,6 +6,22 @@ def b64(string):
 	return str(base64.b64encode(string.encode()), 'utf8')
 def b64str(b64encoded_string):
 	return str(base64.b64decode(b64encoded_string), 'utf8')
+
+def cached(name):
+	"""
+	Decorator used to apply a cacheing mechanism to 
+	an api function
+	"""
+	def f(func):
+		def cached_func(self, identifier):
+			if self.use_cache and identifier in self.cache[name]:
+				return self.cache[name][identifier]
+			result = func(self, identifier)
+			if self.use_cache:
+				self.cache[name][identifier] = result
+			return result
+		return cached_func
+	return f
 	
 class Api:
 	def __init__(self, address, use_cache=True):
@@ -15,6 +31,12 @@ class Api:
 		"""
 		self.base_addr = address;
 		self.use_cache = use_cache;
+		self.cache = {
+			'testsets': {},
+			'metrics' : {},
+			'conf_files': {},
+			'schedulers' : {}
+		}
 	
 	def get_testsets_by_category(self, category=""):
 		"""
@@ -37,39 +59,6 @@ class Api:
 		else:
 			return [];
 		
-		
-	def get_testset_files(self, testset_id):
-		"""
-		Gets a list of XML configuration files for the given test id
-		Only their id is returned.
-		"""
-		r = url.urlopen(self.base_addr + "/app/api/testfiles/" + str(testset_id))
-		if(r.code == 200):
-			val = str(r.read(), encoding='utf8')
-			values = val.rsplit(',')
-			tuples = []
-			for i in range(0, len(values)):
-				tuples.append(
-					 int(values[i]),
-				)
-			return tuples
-			
-		else:
-			return []
-		
-	def get_conf_file(self, file_id):
-		"""
-		Gets the configuration file whose id is file_id.
-		Given as tuple (name, content)
-		"""
-		r = url.urlopen(self.base_addr + "/app/api/conf_file/" + str(file_id))
-		if(r.code == 200):
-			val = str(r.read(), encoding='utf8')
-			values = val.rsplit(',')
-			return (b64str(values[0]), b64str(values[1]))
-			
-		else:
-			return []
 		
 	def get_schedulers_by_code(self, code):
 		"""
@@ -106,25 +95,16 @@ class Api:
 			return tuples
 		else:
 			return [];
-	
-	def get_scheduler_data(self, sched_id):
-		"""
-		Gets the scheduler data given the scheduler id.
-		The scheduler data is a tuple (name, class_name, code).
-		"""
-		r = url.urlopen(self.base_addr + "/app/api/schedulers/data/" + str(sched_id))
-		if(r.code == 200):
-			val = str(r.read(), encoding='utf8')
-			values = [b64str(value) for value in val.rsplit(',')]
-			return (values[0], values[1], values[2]);
-		else:
-			return ""
-	
+			
 	def get_metrics(self, testset_id, scheduler_id):
 		"""Returns a list of metrics ids corresponding to the given test set and scheduler id"""
 		r = url.urlopen(self.base_addr + "/app/api/metrics/" + str(testset_id) + "/" + str(scheduler_id))
 		if(r.code == 200):
 			val = str(r.read(), encoding='utf8')
+			
+			if(val == ''):
+				return []
+				
 			values = val.rsplit(',')
 			tuples = []
 			for i in range(0, len(values)):
@@ -132,7 +112,57 @@ class Api:
 			return tuples
 		else:
 			return []
-
+	
+	@cached('schedulers')
+	def get_scheduler_data(self, identifier):
+		"""
+		Gets the scheduler data given the scheduler id.
+		The scheduler data is a tuple (name, class_name, code).
+		"""
+		r = url.urlopen(self.base_addr + "/app/api/schedulers/data/" + str(identifier))
+		if(r.code == 200):
+			val = str(r.read(), encoding='utf8')
+			values = [b64str(value) for value in val.rsplit(',')]
+			return (values[0], values[1], values[2]);
+		else:
+			return ""
+	
+	@cached('testsets')
+	def get_testset_files(self, identifier):
+		"""
+		Gets a list of XML configuration files for the given test id
+		Only their id is returned.
+		"""
+		r = url.urlopen(self.base_addr + "/app/api/testfiles/" + str(identifier))
+		if(r.code == 200):
+			val = str(r.read(), encoding='utf8')
+			values = val.rsplit(',')
+			tuples = []
+			for i in range(0, len(values)):
+				tuples.append(
+					 int(values[i]),
+				)
+			return tuples
+			
+		else:
+			return []
+	
+	@cached('conf_files')
+	def get_conf_file(self, identifier):
+		"""
+		Gets the configuration file whose id is identifier.
+		Given as tuple (name, content)
+		"""
+		r = url.urlopen(self.base_addr + "/app/api/conf_file/" + str(identifier))
+		if(r.code == 200):
+			val = str(r.read(), encoding='utf8')
+			values = val.rsplit(',')
+			return (b64str(values[0]), b64str(values[1]))
+			
+		else:
+			return []
+	
+	@cached('metrics')
 	def get_metric(self, identifier):
 		"""
 		Gets all the metrics associated to the given metric_id.
@@ -143,13 +173,15 @@ class Api:
 		r = url.urlopen(self.base_addr + "/app/api/metrics/id/" + str(identifier))
 		if(r.code == 200):
 			val = str(r.read(), encoding='utf8')
+			print(val)
 			values = val.rsplit(',')
 			dic = {}
 			array = [int(values[0]), int(values[1]), dic]
-			for i in range(2, (len(values) - 2)//2):
+			for i in range(1, len(values)//2):
 				key = b64str(values[i*2])
 				value = b64str(values[i*2+1])
-				dic.append(key, value)
+				print(key + " -" + value)
+				dic[key] = value
 				
 			return array
 			
