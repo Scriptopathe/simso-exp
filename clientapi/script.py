2ab632f1d8c94237373ea833a5b13d172f942140
Josué A <josue.alvarez@hotmail.fr>
2015-07-27 12:47:58 +0000

    Updated example script

--- clientapi/script.py
+++ clientapi/script.py
@@ -12,49 +12,60 @@ BOLD = '\033[1m'
 UNDERLINE = '\033[4m'
 HEADER = OKBLUE + BOLD
 
-if False:
-	db = SimsoDatabase("http://localhost:8000")
-	print(HEADER + "------ Simso DB test program ------" + ENDC)
-	print(HEADER + "------ All test sets" + ENDC)
-	sets = db.testsets()
-	blbl = db.testsets()
-	print(repr(sets))
-	print(HEADER + "------ One test set" + ENDC)
-	test = sets[0]
-	print(repr(test))
-	print(HEADER + "------ Test set configuration files" + ENDC)
-	conf = test.conf_files
-	print(repr(blbl[0].conf_files))
-	print(repr(conf))
-	print(HEADER + "------ First configuration file" + ENDC)
-	print(repr(conf[0]))
-	print(HEADER + "------ Loaded conf file" + ENDC)
-	print(repr(conf[0].configuration))
-	print(HEADER + "------ EDF Scheduler" + ENDC)
-	sched = db.schedulers("simso.schedulers.EDF")[0]
-	print(HEADER + "------ Metrics" + ENDC)
-	metrics = db.metrics(test.identifier, sched.identifier)
-	print(repr(metrics))
-	print(repr(metrics[0].all()))
-	cf = conf[0].configuration
-	cf.scheduler_info.cls = sched.cls
-	model = Model(cf)
-	model.run_model()
-	print(HEADER + "------ SUCCESSFULLY RUN MODEL" + ENDC)
+def menu(options):
+	for i, opt in enumerate(options):
+		print("{} : {}".format(i, opt))
+	choice = int(input("Select an option : "))
+	return options[choice]
 
-cfg = Configuration("test/test.xml")
-cfg2 = Configuration("test/configuration.xml")
+# Choose the samples to run
+RUN = [1]
+
+
+# Database initialisation
 db = SimsoDatabase("http://localhost:8000")
-for i in range(1, 1000):
-	cfg = Configuration("test/test.xml")
-	cfg2 = Configuration("test/configuration.xml")
-	e = Experiment(db, 
-		(
-			"Custom test", 
-			["testcat1", "testcat2"],
-			[cfg, cfg2],
-		),
-		db.schedulers("superman.schedulers.EDF")[0])
+
+# ----
+# Running an experiment with local configuration files
+# ----
+if 0 in RUN:
+	# Configure the experiment.
+	scheduler_name = "superman.schedulers.EDF"
+	test_name = "sample_test"
+	test_categories = ["sample_category"]
+	files = ["test/test.xml", "test/configuration.xml"]
+	
+	# Create the experiment.
+	config = (test_name, test_categories, [Configuration(f) for f in files])
+	e = Experiment(db, config, db.schedulers(scheduler_name)[0])
+	
+	# Run the experiment
 	e.run()
-	print("Uploading experiment {} / {}".format(i, 1000))	
-	e.upload()
+	
+	# Optional : Upload the experiment
+	# e.upload()
+	
+	# Get the metrics 
+	e.metrics
+	
+	# Get the simulation results
+	e.results
+
+if 1 in RUN:
+	# ----
+	# Running an experiment with a remote test set
+	# ----
+	scheduler_name = "superman.schedulers.EDF"
+	
+	# Gets all the categories
+	categories = db.categories()
+	
+	# Gets all the testset for the choosen category
+	testsets = db.testsets(menu(categories))
+	
+	# Selects a testset
+	testset = menu(testsets)
+	
+	# Create the experiment
+	e = Experiment(db, testset, db.schedulers(scheduler_name)[0])
+	e.run()
