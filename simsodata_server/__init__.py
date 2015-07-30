4c1639502fc5bad9e067f4b61a1fa8fcb08e647c
Josué A <josue.alvarez@hotmail.fr>
2015-07-30 14:39:26 +0000

    Added license information and files

---
+++ simsodata_server/__init__.py
@@ -0,0 +1,8 @@
+__author__ = "Josué Alvarez"
+__copyright__ = "Copyright 2015, LAAS-CNRS"
+__credits__ = ["Josué Alvarez"]
+__license__ = "BSD"
+__version__ = "2"
+__maintainer__ = ""
+__email__ = ""
+__status__ = "Production"
