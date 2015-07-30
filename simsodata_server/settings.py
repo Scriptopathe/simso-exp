489dc40add4de1866be347ec45601ba5ed05a06f
Josué A <josue.alvarez@hotmail.fr>
2015-07-30 13:24:54 +0000

    added bootstrap cdn to allowed hosts

--- simsodata_server/settings.py
+++ simsodata_server/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = ["localhost" , "www.bootstrapcdn.com"]
 LOGIN_REDIRECT_URL = "/accounts/login/"
 LOGOUT_REDIRECT_URL = "/accounts/login/"
 
