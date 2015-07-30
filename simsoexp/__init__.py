df6e1d762631d26d7bb934e0986116ac1364ba3d
Josué A <josue.alvarez@hotmail.fr>
2015-07-30 17:19:00 +0000

    Updated license information

--- simsoexp/__init__.py
+++ simsoexp/__init__.py
@@ -1,6 +1,6 @@
-__author__ = "Josué Alvarez"
+__author__ = "Josue Alvarez"
 __copyright__ = "Copyright 2015, LAAS-CNRS"
-__credits__ = ["Josué Alvarez"]
+__credits__ = ["Josue Alvarez"]
 __license__ = "BSD"
 __version__ = "2"
 __maintainer__ = ""
