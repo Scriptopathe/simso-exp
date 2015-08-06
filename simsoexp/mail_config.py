9533d8ca1d290761722e7d06ef8fd4dcb6b92e21
Josué A <josue.alvarez@hotmail.fr>
2015-08-06 16:52:44 +0000

    Updated documentation

--- simsoexp/mail_config.py
+++ simsoexp/mail_config.py
@@ -1,9 +1,15 @@
+# Put MAIL_ENABLED = True to enable the mail service.
 MAIL_ENABLED = False
+
 # Name of the SMTP mail server (either DSN name or IP address)
+# This is the server that will send the mail.
 MAIL_SERVER = ""
+
 # Port of the mail server
 MAIL_SERVER_PORT = 587
+
 # Email address of the mail sender
 MAIL_SENDER_ADDRESS = ""
+
 # Password of the mail sender
 MAIL_SENDER_PASSWORD = ""
