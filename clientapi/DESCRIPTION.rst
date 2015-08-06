7e04e5dfdf3ef2ff2fc99eebe034de01f4607ce9
Josué A <josue.alvarez@hotmail.fr>
2015-08-06 18:15:57 +0000

    Updated release files

--- clientapi/DESCRIPTION.rst
+++
@@ -1,2 +0,0 @@
-This package contains the client API used to interact with Simso Experiment Platform instances.
-See https://github.com/Scriptopathe/simso-exp for further details.
--- clientapi/setup.py
+++ clientapi/setup.py
@@ -13,26 +13,23 @@ from os import path
 
 here = path.abspath(path.dirname(__file__))
 
-# Get the long description from the relevant file
-with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
-    long_description = f.read()
-
 setup(
-    name='simsoexp.clientapi',
+    name='simsoexp',
 
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.0',
+    version='1.0.4',
 
     description='Simso Experiment Plaform client API',
-    long_description=long_description,
+    long_description="""This package contains the client API used to interact with Simso Experiment Platform instances.
+See https://github.com/Scriptopathe/simso-exp for further details.""",
 
     # The project's main homepage.
     url='https://github.com/Scriptopathe/simso-exp',
 
     # Author details
-    author='Josue Alvaezz',
+    author='Josue Alvaez',
     author_email='josue.alvarez.dev@outlook.com',
 
     # Choose your license
@@ -66,7 +63,7 @@ setup(
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    packages=["simsoexp", 'simsoexp.clientapi'],
 
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
