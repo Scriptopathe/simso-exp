0e12e0015052bd662bd97f18ca1672acd75b69e5
Josué A <josue.alvarez@hotmail.fr>
2015-08-06 15:34:17 +0000

    Updated comments

--- simsoexp/mail_config.py
+++ simsoexp/mail_config.py
@@ -1,5 +1,5 @@
 MAIL_ENABLED = False
-# Name of the mail server (either DSN name or IP address)
+# Name of the SMTP mail server (either DSN name or IP address)
 MAIL_SERVER = ""
 # Port of the mail server
 MAIL_SERVER_PORT = 587
